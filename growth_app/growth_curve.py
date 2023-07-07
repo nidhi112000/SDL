86a679d4f30d1e4040b9fabff45345b070001541
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-07-07 14:42:27 -0500

    refactor

--- growth_app/growth_curve.py
+++ growth_app/growth_curve.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-#from rpl_wei.wei_workcell_base import WEI
+
+# from rpl_wei.wei_workcell_base import WEI
 from tools.c2_flow import c2_flow
 from pathlib import Path
 from workflows.growth_curve.hso_functions import package_hso
@@ -9,49 +10,60 @@ from workflows.growth_curve import solo_step1, solo_step2, solo_step3
 from rpl_wei import Experiment
 import time
 
+
 def main():
-    wf_path_1 = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/create_plate_T0.yaml')
-    wf_path_2 = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/read_plate_T12.yaml')
-    exp = Experiment('127.0.0.1', '8000', 'Growth_Curve')
-    exp.register_exp() 
-    payload={
-        'temp': 37.0, 
-        'humidity': 95.0,
-        'shaker_speed': 30,
-        "stacker": 1, 
+    wf_path_1 = Path(
+        "/home/rpl/workspace/BIO_workcell/growth_app/workflows/growth_curve/create_plate_T0.yaml"
+    )
+    wf_path_2 = Path(
+        "/home/rpl/workspace/BIO_workcell/growth_app/workflows/growth_curve/read_plate_T12.yaml"
+    )
+    exp = Experiment("127.0.0.1", "8000", "Growth_Curve")
+    exp.register_exp()
+    payload = {
+        "temp": 37.0,
+        "humidity": 95.0,
+        "shaker_speed": 30,
+        "stacker": 1,
         "slot": 1,
-        "treatment": "col1", # string of treatment name. Ex. "col1", "col2"
+        "treatment": "col1",  # string of treatment name. Ex. "col1", "col2"
         "culture_column": 1,  # int of cell culture column. Ex. 1, 2, 3, etc.
-        "culture_dil_column": 1, # int of dilution column for 1:10 culture dilutions. Ex. 1, 2, 3, etc.
+        "culture_dil_column": 1,  # int of dilution column for 1:10 culture dilutions. Ex. 1, 2, 3, etc.
         "media_start_column": 1,  # int of column to draw media from (requires 2 columns, 1 means columns 1 and 2) Ex. 1, 3, 5, etc.
-        "treatment_dil_half": 1,  #  int of which plate half to use for treatment serial dilutions. Options are 1 or 2. 
-        "tip_box_position" : 3,
-        }
+        "treatment_dil_half": 1,  #  int of which plate half to use for treatment serial dilutions. Options are 1 or 2.
+        "tip_box_position": 3,
+    }
 
     # from somewhere import create_hso? or directly the solo script
     exp.events.log_local_compute("package_hso")
-    hso_1, hso_1_lines, hso_1_basename = package_hso(solo_step1.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp1.hso") 
-    hso_2, hso_2_lines, hso_2_basename = package_hso(solo_step2.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp2.hso")  
-    hso_3, hso_3_lines, hso_3_basename = package_hso(solo_step3.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp3.hso")  
+    hso_1, hso_1_lines, hso_1_basename = package_hso(
+        solo_step1.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp1.hso"
+    )
+    hso_2, hso_2_lines, hso_2_basename = package_hso(
+        solo_step2.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp2.hso"
+    )
+    hso_3, hso_3_lines, hso_3_basename = package_hso(
+        solo_step3.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp3.hso"
+    )
 
     # update payload with solo hso details
-    payload['hso_1'] = hso_1
-    payload['hso_1_lines'] = hso_1_lines
-    payload['hso_1_basename'] = hso_1_basename
+    payload["hso_1"] = hso_1
+    payload["hso_1_lines"] = hso_1_lines
+    payload["hso_1_basename"] = hso_1_basename
 
-    payload['hso_2'] = hso_2
-    payload['hso_2_lines'] = hso_2_lines
-    payload['hso_2_basename'] = hso_2_basename
+    payload["hso_2"] = hso_2
+    payload["hso_2_lines"] = hso_2_lines
+    payload["hso_2_basename"] = hso_2_basename
 
-    payload['hso_3'] = hso_3
-    payload['hso_3_lines'] = hso_3_lines
-    payload['hso_3_basename'] = hso_3_basename
-    
+    payload["hso_3"] = hso_3
+    payload["hso_3_lines"] = hso_3_lines
+    payload["hso_3_basename"] = hso_3_basename
+
     # #run Growth Create Plate
     flow_info = exp.run_job(wf_path_1.resolve(), payload=payload, simulate=False)
 
     flow_status = exp.query_job(flow_info["job_id"])
-    while(flow_status["status"] != "finished" and flow_status["status"] != "failure"):
+    while flow_status["status"] != "finished" and flow_status["status"] != "failure":
         flow_status = exp.query_job(flow_info["job_id"])
         time.sleep(3)
 
@@ -65,7 +77,7 @@ def main():
     # flow_title = Path(hidex_file_path) #Path(run_info["hist"]["run_assay"]["step_response"])
     # fname = flow_title.name
     # flow_title = flow_title.parents[0]
-  
+
     # c2_flow("hidex_test", str(fname.split('.')[0]), hidex_file_path, flow_title, fname, exp)
     # #wait while incubating
     # # time.sleep(43200)
@@ -97,7 +109,7 @@ def main():
     # flow_title = Path(hidex_file_path) #Path(run_info["hist"]["run_assay"]["step_response"])
     # fname = flow_title.name
     # flow_title = flow_title.parents[0]
-  
+
 
 if __name__ == "__main__":
     main()
--- growth_app/hidex_debugging_run.py
+++ growth_app/hidex_debugging_run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-#from rpl_wei.wei_workcell_base import WEI
+
+# from rpl_wei.wei_workcell_base import WEI
 from tools.c2_flow import c2_flow
 from pathlib import Path
 from workflows.growth_curve.hso_functions import package_hso
@@ -9,36 +10,47 @@ from workflows.growth_curve import solo_step1, solo_step2, solo_step3
 from rpl_wei import Experiment
 import time
 
-def main():
-    wf_path_1 = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/create_plate_T0.yaml')
-    hidex_open = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/hidex_open.yaml')
-    hidex_run = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/hidex_run.yaml')
-    hidex_close = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/hidex_close.yaml')
-    peeler_peel = Path('/home/rpl/workspace/BIO_workcell/bio_workcell/workflows/growth_curve/peeler_peel.yaml')
 
-    exp = Experiment('127.0.0.1', '8000', 'Hidex_Test')
-    exp.register_exp() 
+def main():
+    wf_path_1 = Path(
+        "/home/rpl/workspace/BIO_workcell/growth_app/workflows/growth_curve/create_plate_T0.yaml"
+    )
+    hidex_open = Path(
+        "/home/rpl/workspace/BIO_workcell/growth_app/workflows/growth_curve/hidex_open.yaml"
+    )
+    hidex_run = Path(
+        "/home/rpl/workspace/BIO_workcell/growth_app/workflows/growth_curve/hidex_run.yaml"
+    )
+    hidex_close = Path(
+        "/home/rpl/workspace/BIO_workcell/growth_app/workflows/growth_curve/hidex_close.yaml"
+    )
+    peeler_peel = Path(
+        "/home/rpl/workspace/BIO_workcell/growth_app/workflows/growth_curve/peeler_peel.yaml"
+    )
+
+    exp = Experiment("127.0.0.1", "8000", "Hidex_Test")
+    exp.register_exp()
 
     for i in range(2):
         # payload={
-        #     'temp': 37.0, 
+        #     'temp': 37.0,
         #     'humidity': 95.0,
         #     'shaker_speed': 30,
-        #     "stacker": 1, 
+        #     "stacker": 1,
         #     "slot": 1,
         #     "treatment": "col1", # string of treatment name. Ex. "col1", "col2"
         #     "culture_column": 1,  # int of cell culture column. Ex. 1, 2, 3, etc.
         #     "culture_dil_column": 1, # int of dilution column for 1:10 culture dilutions. Ex. 1, 2, 3, etc.
         #     "media_start_column": 1,  # int of column to draw media from (requires 2 columns, 1 means columns 1 and 2) Ex. 1, 3, 5, etc.
-        #     "treatment_dil_half": 1,  #  int of which plate half to use for treatment serial dilutions. Options are 1 or 2. 
+        #     "treatment_dil_half": 1,  #  int of which plate half to use for treatment serial dilutions. Options are 1 or 2.
         #     "tip_box_position" : 3,
         #     }
 
         # # from somewhere import create_hso? or directly the solo script
         # exp.events.log_local_compute("package_hso")
-        # hso_1, hso_1_lines, hso_1_basename = package_hso(solo_step1.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp1.hso") 
-        # hso_2, hso_2_lines, hso_2_basename = package_hso(solo_step2.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp2.hso")  
-        # hso_3, hso_3_lines, hso_3_basename = package_hso(solo_step3.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp3.hso")  
+        # hso_1, hso_1_lines, hso_1_basename = package_hso(solo_step1.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp1.hso")
+        # hso_2, hso_2_lines, hso_2_basename = package_hso(solo_step2.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp2.hso")
+        # hso_3, hso_3_lines, hso_3_basename = package_hso(solo_step3.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp3.hso")
 
         # # update payload with solo hso details
         # payload['hso_1'] = hso_1
@@ -82,7 +94,9 @@ def main():
         # Open Hidex
         flow_info = exp.run_job(hidex_open.resolve(), payload=None, simulate=False)
         flow_status = exp.query_job(flow_info["job_id"])
-        while(flow_status["status"] != "finished" and flow_status["status"] != "failure"):
+        while (
+            flow_status["status"] != "finished" and flow_status["status"] != "failure"
+        ):
             flow_status = exp.query_job(flow_info["job_id"])
             time.sleep(3)
 
@@ -91,11 +105,13 @@ def main():
 
         print(run_info)
 
-        #Close Hidex
+        # Close Hidex
         flow_info = exp.run_job(hidex_close.resolve(), payload=None, simulate=False)
 
         flow_status = exp.query_job(flow_info["job_id"])
-        while(flow_status["status"] != "finished" and flow_status["status"] != "failure"):
+        while (
+            flow_status["status"] != "finished" and flow_status["status"] != "failure"
+        ):
             flow_status = exp.query_job(flow_info["job_id"])
             time.sleep(3)
 
@@ -120,7 +136,7 @@ def main():
         # flow_title = Path(hidex_file_path) #Path(run_info["hist"]["run_assay"]["step_response"])
         # fname = flow_title.name
         # flow_title = flow_title.parents[0]
-    
+
         # c2_flow("hidex_test", str(fname.split('.')[0]), hidex_file_path, flow_title, fname, exp)
 
         # if i == 0:
@@ -132,7 +148,7 @@ def main():
         #         minutes = int(((deltaSeconds - hours*3600) - (deltaSeconds - hours*3600) % 60)/60)
         #         seconds = deltaSeconds - hours*3600 - minutes * 60
         #         print("Time Since Start: ", hours, " Hours, ", minutes, " Minutes, ", seconds, " Seconds")
-  
+
 
 if __name__ == "__main__":
     main()
