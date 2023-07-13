913e8e9fda4054b5c18590cd5c698aa45ccb425c
Casey Stone <cstone@anl.gov>
2023-07-13 15:12:56 -0500

    fix!

--- scripts/run_wei_server.sh
+++ scripts/run_wei_server.sh
@@ -11,13 +11,12 @@ tmux send-keys -t $session:$window 'cd ' $folder C-m
 tmux send-keys -t $session:$window 'envsubst < redis.conf | redis-server -' C-m
 
 window=1
-tmux new-window -t $session:$window -n 'worker'
+tmux new-window -t $session:$window -n 'server'
 tmux send-keys -t $session:$window 'cd ' $folder C-m
-tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
-tmux send-keys -t $session:$window 'python3 -m rpl_wei.processing.worker' C-m
+tmux send-keys -t $session:$window 'python3 -m rpl_wei.server --workcell ~/workspace/BIO_workcell/bio_workcell.yaml' C-m
 
 window=2
-tmux new-window -t $session:$window -n 'server'
+tmux new-window -t $session:$window -n 'worker'
 tmux send-keys -t $session:$window 'cd ' $folder C-m
 tmux send-keys -t $session:$window 'python3 -m rpl_wei.server --workcell ~/workspace/BIO_workcell/bio_workcell.yaml' C-m
 
