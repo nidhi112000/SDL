abb9799fd7ab799a7d3c7635496585a6a17f4d5f
Casey Stone <cstone@anl.gov>
2023-10-19 15:08:05 -0500

    ale debug round 1

--- ale_zah_nidhi/tools/hudson_solo_auxillary/solo_step2.py
+++ ale_zah_nidhi/tools/hudson_solo_auxillary/solo_step2.py
@@ -1,6 +1,6 @@
 
 """
-Generates steps 1, 2, and 3 SOLO hso files given command line inputs
+Generates SOLO hso files given command line inputs
 
 Returns paths to newly generated .hso files
 """
@@ -20,11 +20,7 @@ def generate_hso_file(
     """generate_hso_file
 
     Description: 
-        Generates SOLOSoft .hso file for step 1 of the substrates workflow
-
-        Step 1 of the growth curve protocol includes:
-            - 1:10 dilution of cells from source culture plate
-            - transfer of 1:10 diluted cells into assay plate
+        Generates SOLOSoft .hso file for step 2 of the substrates workflow
 
     Args:
         payload (dict): input variables from the wei workflow
@@ -34,38 +30,21 @@ def generate_hso_file(
         # extract payload variables
     try: 
         # treatment = payload['treatment'] 
-        substrates_start_column = payload['substrates_start_column_2']
-        # tiprack_start_column = payload['tiprack_start_column']
-        # plates_start_column = payload['plates_start_column']
-        # media_start_column = payload['media_start_column']
-        # treatment_dil_half = payload['treatment_dil_half']
+        substrates_start_column_2 = payload['substrates_start_column_2']
+  
     except Exception as error_msg: 
         # TODO: how to handle this?
         raise error_msg
 
 
 # * Other program variables
-    blowoff_volume = 10
-    # num_mixes = 3
     media_z_shift = 0.5
-    reservoir_z_shift = 0.5  # z shift for deep blocks (Deck Positions 3 and 5)
     flat_bottom_z_shift = 2  # Note: 1 is not high enough (tested)
 
     # Step 1 variables
-    substrates_transfer_volume_s1 = 150
-    # culture_transfer_volume_s1 = 30
-    # half_dilution_media_volume = 99
-    # dilution_culture_volume = 22
-    # culture_plate_mix_volume_s1 = 100  # mix volume increased for test 09/07/21
-    # culture_plate_num_mix = 7
-    # culture_dilution_num_mix = 10
-    # growth_plate_mix_volume_s1 = 40
-    # culture_dilution_mix_volume = 180
+    substrates_transfer_volume = 150
 
-    """
-    STEP 1: INNOCULATE GROWTH PLATE FROM SOURCE BACTERIA PLATE -----------------------------------------------------------------
-    """
-    # * Initialize soloSoft (step 1)
+    # * Initialize soloSoft (step 2)
     soloSoft = SoloSoft(
         filename=temp_file_path,
         plateList=[
@@ -83,18 +62,18 @@ def generate_hso_file(
     # * Fill all columns of empty 96 well plate (corning 3383 or Falcon - ref 353916) with fresh substrates (12 channel in Position 1, media_start_column and media_start_column+1)
     soloSoft.getTip("Position1")  
     # j = 1
-    for i in range(1, 13):  # first half plate = media from column 1
+    for i in range(1, 13):  
         soloSoft.aspirate(
             position="Position3",
             aspirate_volumes=DeepBlock_96VWR_75870_792_sterile().setColumn(
-                substrates_start_column_2, substrates_transfer_volume_s1
+                substrates_start_column_2, substrates_transfer_volume
             ),
             aspirate_shift=[0, 0, media_z_shift],
         )
         soloSoft.dispense(
-            position="Position7",
+            position="Position5",
             dispense_volumes=Plate_96_Corning_3635_ClearUVAssay().setColumn(
-                i, substrates_transfer_volume_s1
+                i, substrates_transfer_volume
             ),
             dispense_shift=[0, 0, flat_bottom_z_shift],
         )
