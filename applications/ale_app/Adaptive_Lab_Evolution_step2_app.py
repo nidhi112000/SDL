9413f415e2d52a227a3275276c71e286faf956ac
nidhi112000 <69688384+nidhi112000@users.noreply.github.com>
2024-05-14 14:03:54 -0500

    Update Adaptive_Lab_Evolution_step2_app.py

--- applications/ale_app/Adaptive_Lab_Evolution_step2_app.py
+++ applications/ale_app/Adaptive_Lab_Evolution_step2_app.py
@@ -25,52 +25,25 @@ def main():
     #Generate the payload for the T0 and T12 readings. The T0 and T12 Yaml files (at the position paths above) and hso_packages for the Hudson Solo (created below) will use the following values in the workflow
     payload = {
         # SUBSTRATE LOAD - STEP 1
-        "substrates_start_column_1": 7,  # int of column to draw media from (requires 2 columns, 1 means columns 1 and 2) Ex. 1, 3, 5, etc.
-        "substrates_start_column_2": 8,  # int of column to draw media from (requires 2 columns, 1 means columns 1 and 2) Ex. 1, 3, 5, etc.
-        "substrates_start_column_3": 9,  # int of column to draw media from (requires 2 columns, 1 means columns 1 and 2) Ex. 1, 3, 5, etc.
-        "substrates_start_column_4": 10,  # int of column to draw media from (requires 2 columns, 1 means columns 1 and 2) Ex. 1, 3, 5, etc.
-        "substrates_start_column_5": 11,  # int of column to draw media from (requires 2 columns, 1 means columns 1 and 2) Ex. 1, 3, 5, etc.   
+        "substrates_start_column_1": 3,  # int of column to draw media from (requires 2 columns, 1 means columns 1 and 2) Ex. 1, 3, 5, etc.
+ 
     }
 
     # Creating HSO Packages to send liquid handling protocols to the Hudson Solo.
     exp.events.log_local_compute("package_hso")
-    hso_1, hso_1_lines, hso_1_basename = package_hso(
-        solo_step1.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp1.hso"
-    )
+    
     hso_2, hso_2_lines, hso_2_basename = package_hso(
         solo_step2.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp2.hso"
     )
-    hso_3, hso_3_lines, hso_3_basename = package_hso(
-        solo_step3.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp3.hso"
-    )
-    hso_4, hso_4_lines, hso_4_basename = package_hso(
-        solo_step4.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp4.hso"
-    )
-    hso_5, hso_5_lines, hso_5_basename = package_hso(
-        solo_step5.generate_hso_file, payload, "/home/rpl/wei_temp/solo_temp5.hso"
-    )
-
+    
     # Add the HSO Packages to the payload to send to the Hudson Solo
-    payload["hso_1"] = hso_1
-    payload["hso_1_lines"] = hso_1_lines
-    payload["hso_1_basename"] = hso_1_basename
+   
 
     payload["hso_2"] = hso_2
     payload["hso_2_lines"] = hso_2_lines
     payload["hso_2_basename"] = hso_2_basename
 
-    payload["hso_3"] = hso_3
-    payload["hso_3_lines"] = hso_3_lines
-    payload["hso_3_basename"] = hso_3_basename
-
-    payload["hso_4"] = hso_4
-    payload["hso_4_lines"] = hso_4_lines
-    payload["hso_4_basename"] = hso_4_basename
-
-    payload["hso_5"] = hso_5
-    payload["hso_5_lines"] = hso_5_lines
-    payload["hso_5_basename"] = hso_5_basename
-
+   
     # Run workflow file
     flow_info = exp.run_job(wf_path_1.resolve(), payload=payload, simulate=False)
 
